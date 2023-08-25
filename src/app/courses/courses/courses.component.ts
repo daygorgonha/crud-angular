60c82c5a99435481b19531f7329205456ecc12b6
Dayane Gorgonha <dayanebismark18@gmail.com>
2023-08-25 17:58:01 -0300

    aula 08 - criacao do service no angular

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'Front-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
 }
